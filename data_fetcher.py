e161ed699aa48edcd5017f1e3cb7a2b4a32dca05
yohay13 <yohay13@gmail.com>
2021-10-22 13:22:49 +0300

    call romeltorres alpha

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import math
+import asyncio
+from alpha_vantage.async_support.timeseries import TimeSeries
 import os
 import numpy as np
 import pandas as pd
@@ -58,7 +61,7 @@ def convert_columns_to_adjusted(stock_df):
     return df
 
 
-def get_stock_data_trade_daily_alpha_vantage(ticker, start_time, time):
+async def get_stock_data_trade_daily_alpha_vantage_csv(ticker, start_time, time):
     print(ticker)
     api_key = open(key_path, 'r').read()
     data = retry_get_request(f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={ticker}&apikey={api_key}&outputsize=full&datatype=csv').content
@@ -76,10 +79,31 @@ def get_stock_data_trade_daily_alpha_vantage(ticker, start_time, time):
     return df
 
 
+async def get_stock_data_trade_daily_alpha_vantage(ticker):
+    print(ticker)
+    api_key = open(key_path, 'r').read()
+
+    ts = TimeSeries(key=api_key, output_format='pandas')
+    data, _ = await ts.get_daily_adjusted(ticker, outputsize='full')
+    await ts.close()
+
+    if data is None:
+        return None
+    if '4. close' not in data:
+        return None
+    data['timestamp'] = data.index
+    data = data[['timestamp', '1. open', '2. high', '3. low', '4. close', '5. adjusted close', '6. volume']]
+    data.columns = ["Date", "Open","High","Low","Close","Adjusted_Close","Volume"]
+    data = data.iloc[::-1].reset_index(drop=True)
+    data = data.iloc[-(252*4):].reset_index(drop=True) # TODO: 4 years of data
+    df = convert_columns_to_adjusted(data)
+    return ticker, df
+
+
 def get_data_for_stock(ticker, interval, start_time, time_module):
     # switch call between daily adjusted, TODO: intraday_extended! and weekly adjusted
     if interval == 'D':
-        stock_data = get_stock_data_trade_daily_alpha_vantage(ticker, start_time, time_module)
+        stock_data = get_stock_data_trade_daily_alpha_vantage(ticker)
         if stock_data is None:
             return None
         return stock_data
@@ -94,17 +118,26 @@ def add_earnings_dates_to_stock(stock_df, earnings_json):
     return df['is_earning_days']
 
 
-def get_data_dict_for_multiple_stocks(tickers, interval, time_module):
+def get_data_dict_for_multiple_stocks(tickers, time_module):
     ohlc_intraday = {} # dictionary with ohlc value for each stock
-    start_time = time.time()
-    for ticker in tickers:
-        stock_data = get_data_for_stock(ticker, interval, start_time, time_module)
-        if stock_data is None:
-            continue
+    api_usage_limit_per_minute = 150
+    total_number_of_stocks = len(tickers)
+    total_batches_number = math.ceil(total_number_of_stocks / api_usage_limit_per_minute)
+    all_results = []
+    loop = asyncio.get_event_loop()
+    for i in range(total_batches_number):
+        tasks = [get_stock_data_trade_daily_alpha_vantage(ticker) for ticker in tickers[i*api_usage_limit_per_minute : min((i+1)*api_usage_limit_per_minute, total_number_of_stocks)]]
+        group = asyncio.gather(*tasks)
+        batch_results = loop.run_until_complete(group)
+        time_module.sleep(60)
+        all_results = all_results + batch_results
+    loop.close()
+
+    for result in all_results:
+        ohlc_intraday[result[0]] = result[1]
         # earnings = get_stock_earnings_data(ticker, start_time, time_module)
         # stock_data['is_earning_days'] = add_earnings_dates_to_stock(stock_data, earnings)
-        stock_data['is_earning_days'] = ''  # TODO: once I get how to catch and retry, add the earnings back!
-        ohlc_intraday[ticker] = stock_data
+        ohlc_intraday[result[0]]['is_earning_days'] = ''  # TODO: once I get how to catch and retry, add the earnings back!
     return ohlc_intraday
 
 
